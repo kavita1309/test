6bea2f698fef92cdc6fe851f528928f416c1fba0
kavita1309 <kavita.sonparote@gmail.com>
2022-10-20 14:09:40 +0530

    duplicate message

--- src/index.php
+++ src/index.php
@@ -2,5 +2,6 @@
 echo 'Hello World.Welcome.Have a wonderful day!Good afternoon';
 echo 'Hello World.Welcome.Have a wonderful day!Good afternoon';
 echo 'Have a great day ahead.Happy Diwali';
+echo 'Have a great day ahead.Happy Diwali';
 
 ?>
