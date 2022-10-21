e332da5b8b938866da3eb7fd51f3e0995061e998
kavita1309 <kavita.sonparote@gmail.com>
2022-10-21 14:00:57 +0530

    added code

--- src/index.php
+++ src/index.php
@@ -4,5 +4,15 @@ echo 'Hello World.Welcome.Have a wonderful day!Good afternoon';
 echo 'Have a great day ahead.Happy Diwali';
 echo 'Have a great day ahead.Happy Diwali';
 echo 'hey!';
-
+// Here we can see that all echo
+// statements are executed in the same manner
+  
+$variable = 25;
+echo $variable;
+ECHO $variable;
+EcHo $variable;
+ 
+// but this line will show RUNTIME ERROR as
+// "Undefined Variable"
+echo $VARIABLE
 ?>
