20fb8cc7d218d5b8d19ea71c0929663f6220d8c5
kavita1309 <kavita.sonparote@gmail.com>
2022-10-25 15:38:25 +0530

    delete message

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@ echo 'Hello World.Welcome.Have a wonderful day!Good afternoon';
 echo 'Hello World.Welcome.Have a wonderful day!Good afternoon';
 echo 'Have a great day ahead.Happy Diwali';
 echo 'Have a great day ahead.Happy Diwali';
-echo 'hey!';
+
 // Here we can see that all echo
 // statements are executed in the same manner
   
